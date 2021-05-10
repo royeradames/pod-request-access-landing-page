e9a2bc911dbd0a8a148e911e6d035dbb7d2ced98
royeradames <royeraadames@gmail.com>
2021-05-10 16:58:29 -0400

    📦add form html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
+// images
 import logo from "./assets/desktop/logo.svg";
 import applePodcast from "./assets/desktop/apple-podcast.svg";
 import bgDots from "./assets/desktop/bg-pattern-dots.svg";
@@ -9,6 +11,9 @@ import pocketCast from "./assets/desktop/pocket-casts.svg";
 import spotify from "./assets/desktop/spotify.svg";
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+
   return (
     <main className="main">
       <img src={logo} alt="Pod logo" className="logo" />
@@ -43,6 +48,14 @@ function App() {
           className="podcast__pocket-casts"
         />
       </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          {...(register("email"), { required: true })}
+          value="Email Address"
+        />
+        <p className="form--error">{/* {errors.email?.message} */}</p>
+        <input type="submit" value="Request Access" />
+      </form>
     </main>
   );
 }
