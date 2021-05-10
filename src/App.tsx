071cab3fd1ce1a8f814801af9d648f14d2cc1e30
royeradames <royeraadames@gmail.com>
2021-05-10 16:36:15 -0400

    📦import imgs and add logo html

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
+import logo from "./assets/desktop/logo.svg";
+import applePodcast from "./assets/desktop/apple-podcast.svg";
+import bgDots from "./assets/desktop/bg-pattern-dots.svg";
+import googlePodcast from "./assets/desktop/google-podcasts.svg";
+import imageHost from "./assets/desktop/image-host.jpg";
+import pocketCast from "./assets/desktop/pocket-casts.svg";
+import spotify from "./assets/desktop/spotify.svg";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="main">
+      <img src={logo} alt="Pod logo" className="logo" />
+    </main>
+  );
 }
 
 export default App;
