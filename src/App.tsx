efaab31776c7fe4ddb9bb60de56da0119e15a88f
royeradames <royeraadames@gmail.com>
2021-05-12 15:17:34 -0400

    👌remove background images from app file

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 // images
 import logo from "./assets/desktop/logo.svg";
 import applePodcast from "./assets/desktop/apple-podcast.svg";
-import bgDots from "./assets/desktop/bg-pattern-dots.svg";
 import googlePodcast from "./assets/desktop/google-podcasts.svg";
-import imageHost from "./assets/desktop/image-host.jpg";
 import pocketCast from "./assets/desktop/pocket-casts.svg";
 import spotify from "./assets/desktop/spotify.svg";
 
